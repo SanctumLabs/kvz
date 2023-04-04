3dc3ee61052a97dc414d0a3f271c75bf5a9cb54c
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-04-04 13:05:24 +0300

    feat(models): b-tree node offset lists

--- internal/models/b_node.go
+++ internal/models/b_node.go
@@ -27,7 +27,7 @@ func (node BNode) setHeader(bType, nKeys uint16) {
 // pointers
 
 func (node BNode) getPtr(idx uint16) uint64 {
-	if idx < node.nKeys() {
+	if !(idx < node.nKeys()) {
 		panic("idx is less than number of keys")
 	}
 	pos := HEADER + 8*idx
@@ -35,9 +35,58 @@ func (node BNode) getPtr(idx uint16) uint64 {
 }
 
 func (node BNode) setPtr(idx uint16, val uint64) {
-	if idx < node.nKeys() {
+	if !(idx < node.nKeys()) {
 		panic("idx is less than number of keys")
 	}
 	pos := HEADER + 8*idx
 	binary.BigEndian.PutUint64(node.data[pos:], val)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+func offsetPos(node BNode, idx uint16) uint16 {
+	if !(1 <= idx && idx <= node.nKeys()) {
+		panic("Offset position can not be retrieved")
+	}
+	return HEADER + 8*node.nKeys() + 2*(idx-1)
+}
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	if !(idx <= node.nKeys()) {
+		panic("kvPos> cannot get kv position")
+	}
+	return HEADER + 8*node.nKeys() + 2*node.nKeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	if !(idx < node.nKeys()) {
+		panic("getKey> cannot getKey")
+	}
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos:])
+	return node.data[pos+4:][:klen]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+	if !(idx < node.nKeys()) {
+		panic("getVal> cannot get value")
+	}
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos+0:])
+	vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
+	return node.data[pos+4+klen:][:vlen]
+}
+
+// node size in bytes
+func (node BNode) nBytes() uint16 {
+	return node.kvPos(node.nKeys())
 }

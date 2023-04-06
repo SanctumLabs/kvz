18721493b95b4651fe1d94a5dc2343bd560605ef
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-04-06 04:35:07 +0300

    feat(utils): update leaf nodes
    After looking up the position to insert,
    we need to create a copy of the node with the new key in it.
    
    nodeAppendRange function copies keys from and old node to a new node.
    
    nodeAppendKV function copes a KV pair to the new node.

--- internal/models/utils.go
+++ internal/models/utils.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"bytes"
+	"encoding/binary"
 )
 
 // nodeLookupLE returns the first kid node whose range intersects the key. (kid[i] <= key)
@@ -25,4 +26,63 @@ func NodeLookupLE(node BNode, key []byte) uint16 {
 	}
 
 	return found
+}
+
+func leafInsert(new BNode, old BNode, idx uint16, key []byte, val []byte) {
+	new.setHeader(BNODE_LEAF, old.nKeys()+1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKV(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx, old.nKeys()-idx)
+}
+
+// nodeAppendRange copies multiple KVs into the position
+func nodeAppendRange(new BNode, old BNode, dstNew uint16, srcOld uint16, n uint16) {
+	if !(srcOld+n <= old.nKeys()) {
+		panic("srcOld +n > old.nKeys()")
+	}
+
+	if !(dstNew+n <= new.nKeys()) {
+		panic("dstNew+n > new.nKeys()")
+	}
+
+	if n == 0 {
+		return
+	}
+
+	// pointers
+	for i := uint16(0); i < n; i++ {
+		new.setPtr(dstNew+i, old.getPtr(srcOld+i))
+	}
+
+	//offsets
+	dstBegin := new.getOffset(dstNew)
+	srcBegin := old.getOffset(srcOld)
+
+	// Note: the range is [1,n]
+	for i := uint16(1); i <= n; i++ {
+		offset := dstBegin + old.getOffset(srcOld+i) - srcBegin
+		new.setOffset(dstNew+i, offset)
+	}
+
+	// KVs
+	begin := old.kvPos(srcOld)
+	end := old.kvPos(srcOld + n)
+	copy(new.data[new.kvPos(dstNew):], old.data[begin:end])
+}
+
+// nodeAppendKV copies a KV pair to the new node
+func nodeAppendKV(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
+	// pointers
+	new.setPtr(idx, ptr)
+
+	// KVs
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new.data[pos+0:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new.data[pos+2:], uint16(len(val)))
+
+	copy(new.data[pos+4:], key)
+	copy(new.data[pos+4+uint16(len(key)):], val)
+
+	// the offset of the next key
+	new.setOffset(idx+1, new.getOffset(idx)+4+uint16((len(key)+len(val))))
 }
